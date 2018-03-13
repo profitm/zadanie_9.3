2e7ec373933f56f88aca960941c0363a2882a102
Michał Sawicki <sawicki_michal@wp.pl>
2018-03-13 17:56:17 +0100

    update function name

--- js/script.js
+++ js/script.js
@@ -1,19 +1,16 @@
-var text = 'Velociraptor is a genus of herbivorous ceratopsid dinosaur that first appeared during the late Maastrichtian stage of the late Cretaceous period.'
-var dinosaur = 'triceratops'
+var text = 'Velociraptor is a genus of herbivorous ceratopsid dinosaur that first appeared during the late Maastrichtian stage of the late Cretaceous period.';
+var dinosaur = 'triceratops';
 
 var dinosaurUpperCased = dinosaur.toUpperCase();
 
-var textCharsAfter = text.replace('Velociraptor', dinosaurUpperCased);
+var text = text.replace('Velociraptor', dinosaurUpperCased);
 
-
-
-
-function firstHalf(textCharsAfter) {
- if (textCharsAfter.length % 2 == 0) {
-  return textCharsAfter.slice(0, textCharsAfter.length / 2);
+function firstHalf(text) {
+ if (text.length % 2 == 0) {
+  return text.slice(0, text.length / 2);
  }
- return textCharsAfter;
+ return text;
 }
-console.log(firstHalf(textCharsAfter));
+console.log(firstHalf(text));
 
-document.write(firstHalf(textCharsAfter)
+document.write(firstHalf(text));
